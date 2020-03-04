ced1640a5fe42a0170d1b3207f7f76966caebbcb
saikrishna <saikrishna.4369@gmail.com>
2020-03-04 22:09:19 +0100

    update

--- app/src/main/java/com/example/youcare/MainActivity.java
+++ app/src/main/java/com/example/youcare/MainActivity.java
@@ -8,21 +8,33 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int LaunchScreenTime = 2500;
+    protected int LaunchScreenTime = 2500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Intent loginScreen = new Intent(this, LoginScreenActivity.class);
+//        startActivity(loginScreen);
+
+
+//        Intent homeIntent = new Intent(this, LoginScreenActivity.class);
+//        startActivity(homeIntent);
+
+        /*
+        Here a new handler is started, which is useful to display the launch screen for certain period of time.
+         */
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent homeIntent = new Intent(MainActivity.this, LoginScreenActivity.class);
-                startActivity(homeIntent);
-                finish();
+                Intent ApplicationIntroIntent = new Intent(MainActivity.this, AppInfoWhileStartup.class);
+                startActivity(ApplicationIntroIntent);
+//            @method finish() is used to exit the applicaiton when back button is pressed after the launch screen is done, which means the launch screen is fired when the application is
+                //newly opened
+              finish();
             }
-        },LaunchScreenTime);
+        }, LaunchScreenTime);
+
+
     }
-    /*
-    @method
-     */
 }
