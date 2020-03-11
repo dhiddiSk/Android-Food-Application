2788ca56463549c42c757f30a7606d8ed03d6460
saikrishna <saikrishna.4369@gmail.com>
2020-03-11 20:22:48 +0100

    update

---
+++ app/src/main/java/com/example/youcare/shoppingSearchBar.java
@@ -0,0 +1,14 @@
+package com.example.youcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class shoppingSearchBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping_search_bar);
+    }
+}
