efaa4d7559db63d692344bd473524742788d79c7
saikrishna <saikrishna.4369@gmail.com>
2020-04-22 12:33:59 +0200

    update

--- app/src/main/java/com/example/youcare/appBodyNavigation/appBodyPreference/AppBodyNavigationpreferenceFragment.java
+++ app/src/main/java/com/example/youcare/appBodyNavigation/appBodyPreference/AppBodyNavigationpreferenceFragment.java
@@ -1,5 +1,6 @@
 package com.example.youcare.appBodyNavigation.appBodyPreference;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 
 import android.app.Fragment;
@@ -8,14 +9,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.example.youcare.R;
 import com.example.youcare.appBodyNavigation.appBodyPreference.PrefrenceChildActivityOne;
 
-public class AppBodyNavigationpreferenceFragment extends Fragment {
+public class AppBodyNavigationpreferenceFragment extends Fragment implements View.OnClickListener {
 
     ImageButton nextpage;
+    Button vegetarian;
+    Button vegan;
+    Button glutenfree;
+    Button lacktosefree;
+    Button norestrictions;
+
 
 
     @Nullable
@@ -23,16 +31,53 @@ public class AppBodyNavigationpreferenceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_app_body_navigation_preference_fragment, container, false);
 
+
+        vegetarian = view.findViewById(R.id.Vegetarian);
+        vegan = view.findViewById(R.id.Vegan);
+        glutenfree = view.findViewById(R.id.GlutenFree);
+        lacktosefree = view.findViewById(R.id.LacktoseFree);
+        norestrictions = view.findViewById(R.id.Norestrictions);
+        nextpage.setOnClickListener(this);
+        vegetarian.setOnClickListener(this);
+        vegan.setOnClickListener(this);
+        glutenfree.setOnClickListener(this);
+        lacktosefree.setOnClickListener(this);
+        norestrictions.setOnClickListener(this);
+        nextpage = view.findViewById(R.id.nextPage);
+
         return view;
     }
+
+
+    @Override
+    public void onClick(View v) {
+
+            switch (v.getId()){
+
+                case R.id.Vegetarian : vegetarian.setBackgroundColor("@colors/");
+
+
+
+            }
+
+
+
+    }
+
+
 
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        nextpage = view.findViewById(R.id.nextPage);
+
     }
+
+
+
+
+
 
     @Override
     public void onResume() {
