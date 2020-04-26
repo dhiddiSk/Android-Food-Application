8a9e20ea6866f1fe0c6009c4838cc9bf33cfb377
saikrishna <saikrishna.4369@gmail.com>
2020-04-26 10:41:41 +0200

    update

--- app/src/main/java/com/example/youcare/EatingPreferences.java
+++ app/src/main/java/com/example/youcare/EatingPreferences.java
@@ -1,240 +1,240 @@
-package com.example.youcare;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.Toast;
-
-import com.example.youcare.appbody.search.Searchproducts;
-import com.example.youcare.database.DatabaseConnectivity;
-
-/*
-This class refers to the eating habits of the user like vegan and vegetarian.
- */
-
-
-public class EatingPreferences extends AppCompatActivity {
-
-
-    Switch regionalOne, regionalTwo, seasonalOne, seasonalTwo, veganOne, veganTwo, veganThree, vegOne, vegTwo, vegThree;
-    Button applyFilters;
-
-
-   // @param  tempButtonCounter refers to count the user operations on the switches and used to notify the user if none is selected
-    int tempButtonCounter =0;
-
-
-    boolean regionalUserInput, seasonalUserInput;
-    boolean veganUserInput;
-    boolean vegUserInput;
-
-    DatabaseConnectivity database;
-
-    Searchproducts searchObj = new Searchproducts();
-    String milktable = "milk";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_users_preferences);
-        regionalOne = findViewById(R.id.RegionalSwitch1);
-       // regionalTwo = findViewById(R.id.RegionalSwitch2);
-        regionalTwo = findViewById(R.id.RegionalSwitch3);
-        seasonalOne = findViewById(R.id.seasonalSwitch1);
-       // sesonalTwo = findViewById(R.id.seasonalSwitch2);
-        seasonalTwo = findViewById(R.id.seasonalswitch3);
-        veganOne = findViewById(R.id.veganSwitch1);
-        veganTwo = findViewById(R.id.veganSwitch2);
-        veganThree = findViewById(R.id.veganSwitch3);
-        vegOne = findViewById(R.id.vegetarianSwitch1);
-        vegTwo = findViewById(R.id.vegetarianSwitch2);
-        vegThree = findViewById(R.id.vegetarianSwitch3);
-        applyFilters = findViewById(R.id.applyFilters);
-
-        database=new DatabaseConnectivity(this);
-
-        veganOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // The toggle is enabled
-                    tempButtonCounter++;
-                    //Now disable the first button of the nonVeg, i.e nonVeg = true;
-                    veganTwo.setChecked(false);
-                    veganThree.setChecked(false);
-                    vegOne.setEnabled(false);
-                    vegThree.setEnabled(false);
-
-                    veganUserInput = true;
-
-                }
-                if (!isChecked) {
-                    // this is something which is vice versa to the above if condition
-                    vegOne.setEnabled(true);
-                    vegThree.setEnabled(true);
-                }
-            }
-        });
-
-        veganTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // The toggle is enabled
-                    tempButtonCounter++;
-                    //Now disable the first button of the nonVeg, i.e nonVeg = true;
-                    veganOne.setChecked(false);
-                    veganThree.setChecked(false);
-
-                    veganUserInput = true;
-
-
-                }
-            }
-        });
-
-        veganThree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // The toggle is enabled
-                    tempButtonCounter++;
-                    //Now disable the first button of the nonVeg, i.e nonVeg = true;
-                    veganOne.setChecked(false);
-                    veganTwo.setChecked(false);
-
-                    veganUserInput = false;
-                }
-//                if(!isChecked){
+//package com.example.youcare;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.CompoundButton;
+//import android.widget.Switch;
+//import android.widget.Toast;
+//
+//import com.example.youcare.appbody.search.Searchproducts;
+//import com.example.youcare.database.DatabaseConnectivity;
+//
+///*
+//This class refers to the eating habits of the user like vegan and vegetarian.
+// */
+//
+//
+//public class EatingPreferences extends AppCompatActivity {
+//
+//
+//    Switch regionalOne, regionalTwo, seasonalOne, seasonalTwo, veganOne, veganTwo, veganThree, vegOne, vegTwo, vegThree;
+//    Button applyFilters;
+//
+//
+//   // @param  tempButtonCounter refers to count the user operations on the switches and used to notify the user if none is selected
+//    int tempButtonCounter =0;
+//
+//
+//    boolean regionalUserInput, seasonalUserInput;
+//    boolean veganUserInput;
+//    boolean vegUserInput;
+//
+//    DatabaseConnectivity database;
+//
+//    Searchproducts searchObj = new Searchproducts();
+//    String milktable = "milk";
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_users_preferences);
+//        regionalOne = findViewById(R.id.RegionalSwitch1);
+//       // regionalTwo = findViewById(R.id.RegionalSwitch2);
+//        regionalTwo = findViewById(R.id.RegionalSwitch3);
+//        seasonalOne = findViewById(R.id.seasonalSwitch1);
+//       // sesonalTwo = findViewById(R.id.seasonalSwitch2);
+//        seasonalTwo = findViewById(R.id.seasonalswitch3);
+//        veganOne = findViewById(R.id.veganSwitch1);
+//        veganTwo = findViewById(R.id.veganSwitch2);
+//        veganThree = findViewById(R.id.veganSwitch3);
+//        vegOne = findViewById(R.id.vegetarianSwitch1);
+//        vegTwo = findViewById(R.id.vegetarianSwitch2);
+//        vegThree = findViewById(R.id.vegetarianSwitch3);
+//        applyFilters = findViewById(R.id.applyFilters);
+//
+//        database=new DatabaseConnectivity(this);
+//
+//        veganOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // The toggle is enabled
+//                    tempButtonCounter++;
+//                    //Now disable the first button of the nonVeg, i.e nonVeg = true;
+//                    veganTwo.setChecked(false);
+//                    veganThree.setChecked(false);
+//                    vegOne.setEnabled(false);
+//                    vegThree.setEnabled(false);
+//
+//                    veganUserInput = true;
+//
+//                }
+//                if (!isChecked) {
 //                    // this is something which is vice versa to the above if condition
-//                    nonvegOne.setEnabled(true);
+//                    vegOne.setEnabled(true);
+//                    vegThree.setEnabled(true);
 //                }
-            }
-        });
-
-
-        vegOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // The toggle is enabled
-                    tempButtonCounter++;
-                    //Now disable the nonVeg button
-                    veganOne.setEnabled(false);
-                    vegTwo.setChecked(false);
-                    vegThree.setChecked(false);
-                }
-
-                if (!isChecked) {
-                    veganOne.setEnabled(true);
-                }
-
-
-//                else {
-//                    // The toggle is disabled
-//                     veganOne.setEnabled(true);
-//                    //Do not disable the nonveg button
+//            }
+//        });
+//
+//        veganTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // The toggle is enabled
+//                    tempButtonCounter++;
+//                    //Now disable the first button of the nonVeg, i.e nonVeg = true;
+//                    veganOne.setChecked(false);
+//                    veganThree.setChecked(false);
+//
+//                    veganUserInput = true;
+//
+//
 //                }
-            }
-        });
-
-        vegTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
-                    vegOne.setChecked(false);
-                    vegThree.setChecked(false);
-                }
-            }
-        });
-
-        vegThree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
-                    tempButtonCounter++;
-                    vegOne.setChecked(false);
-                    vegTwo.setChecked(false);
-                }
-            }
-        });
-
-        regionalOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
-                    tempButtonCounter++;
-                    regionalTwo.setChecked(false);
-                    //regionalTwo.setChecked(false);
-
-                    regionalUserInput = true;
-                }
-            }
-        });
-
-
-        regionalTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
-                    tempButtonCounter++;
-                   // regionalTwo.setChecked(false);
-                    regionalOne.setChecked(false);
-
-                    regionalUserInput = false;
-                }
-            }
-        });
-
-        seasonalOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
-                    tempButtonCounter++;
-                   // sesonalTwo.setChecked(false);
-                    seasonalTwo.setChecked(false);
-                    seasonalUserInput = true;
-                }
-            }
-        });
-
-        seasonalTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
-                    tempButtonCounter++;
-                    seasonalOne.setChecked(false);
-                    seasonalUserInput = false;
-                   // sesonalTwo.setChecked(false);
-                }
-            }
-        });
-
-        applyFilters.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(EatingPreferences.this, UserValues.class);
-
-                if(tempButtonCounter++ == 0){
-
-                    Toast.makeText(EatingPreferences.this, "Please match your preferences!", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    //new Searchproducts(regionalUserInput,seasonalUserInput, veganUserInput);
-                  //  searchObj.setEatingHabits(regionalUserInput, veganUserInput);
-                    startActivity(intent);
-                }
-
-            }
-
-
-        });
-    }
-}
+//            }
+//        });
+//
+//        veganThree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // The toggle is enabled
+//                    tempButtonCounter++;
+//                    //Now disable the first button of the nonVeg, i.e nonVeg = true;
+//                    veganOne.setChecked(false);
+//                    veganTwo.setChecked(false);
+//
+//                    veganUserInput = false;
+//                }
+////                if(!isChecked){
+////                    // this is something which is vice versa to the above if condition
+////                    nonvegOne.setEnabled(true);
+////                }
+//            }
+//        });
+//
+//
+//        vegOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // The toggle is enabled
+//                    tempButtonCounter++;
+//                    //Now disable the nonVeg button
+//                    veganOne.setEnabled(false);
+//                    vegTwo.setChecked(false);
+//                    vegThree.setChecked(false);
+//                }
+//
+//                if (!isChecked) {
+//                    veganOne.setEnabled(true);
+//                }
+//
+//
+////                else {
+////                    // The toggle is disabled
+////                     veganOne.setEnabled(true);
+////                    //Do not disable the nonveg button
+////                }
+//            }
+//        });
+//
+//        vegTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
+//                    vegOne.setChecked(false);
+//                    vegThree.setChecked(false);
+//                }
+//            }
+//        });
+//
+//        vegThree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
+//                    tempButtonCounter++;
+//                    vegOne.setChecked(false);
+//                    vegTwo.setChecked(false);
+//                }
+//            }
+//        });
+//
+//        regionalOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
+//                    tempButtonCounter++;
+//                    regionalTwo.setChecked(false);
+//                    //regionalTwo.setChecked(false);
+//
+//                    regionalUserInput = true;
+//                }
+//            }
+//        });
+//
+//
+//        regionalTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
+//                    tempButtonCounter++;
+//                   // regionalTwo.setChecked(false);
+//                    regionalOne.setChecked(false);
+//
+//                    regionalUserInput = false;
+//                }
+//            }
+//        });
+//
+//        seasonalOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
+//                    tempButtonCounter++;
+//                   // sesonalTwo.setChecked(false);
+//                    seasonalTwo.setChecked(false);
+//                    seasonalUserInput = true;
+//                }
+//            }
+//        });
+//
+//        seasonalTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    // Since the user is non vegetarian then, user has an option of choosing and not choosing the veg and vegan food.
+//                    tempButtonCounter++;
+//                    seasonalOne.setChecked(false);
+//                    seasonalUserInput = false;
+//                   // sesonalTwo.setChecked(false);
+//                }
+//            }
+//        });
+//
+//        applyFilters.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(EatingPreferences.this, UserValues.class);
+//
+//                if(tempButtonCounter++ == 0){
+//
+//                    Toast.makeText(EatingPreferences.this, "Please match your preferences!", Toast.LENGTH_SHORT).show();
+//                }
+//                else{
+//                    //new Searchproducts(regionalUserInput,seasonalUserInput, veganUserInput);
+//                  //  searchObj.setEatingHabits(regionalUserInput, veganUserInput);
+//                    startActivity(intent);
+//                }
+//
+//            }
+//
+//
+//        });
+//    }
+//}
--- app/src/main/java/com/example/youcare/UserValues.java
+++ app/src/main/java/com/example/youcare/UserValues.java
@@ -1,99 +1,99 @@
-package com.example.youcare;
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RatingBar;
-import android.widget.TextView;
-
-import com.example.youcare.appbody.search.Searchproducts;
-
-public class UserValues extends AppCompatActivity {
-
-    public RatingBar environement, animalTreatment, health, fairAndSocial;
-    public android.widget.SeekBar price;
-    public Button toshop;
-    int environmentUserRating, animalTreatmentUserRating, healthUserRating, fairAndSocialUserRating;
-
-
-//@parameter userPriceInput refers to the input from the user and @parameter priceOfpurchase to show the price on display
-    public int userPriceInput=0;  public TextView priceOfpurchase;
-    Searchproducts Obj2 = new Searchproducts();
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_values);
-        environement = findViewById(R.id.environmentRatingbar);
-        animalTreatment = findViewById(R.id.animaltreatmentRating);
-        health = findViewById(R.id.HealthId);
-        fairAndSocial = findViewById(R.id.FairAndSocial);
-        price = (android.widget.SeekBar) findViewById(R.id.PriceSeekbar);
-        toshop = findViewById(R.id.shoppingButton);
-        priceOfpurchase = findViewById(R.id.priceView);
-
-        //priceOfpurchase.setText(userPriceInput)
-
-
-        price.setOnSeekBarChangeListener(new android.widget.SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(android.widget.SeekBar seekBar, int progress, boolean fromUser) {
-                userPriceInput = progress;
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(android.widget.SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(android.widget.SeekBar seekBar) {
-                String temp = Integer.toString(userPriceInput);
-                priceOfpurchase.setText(temp);
-
-            }
-       });
-
-        environement.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                environmentUserRating = Math.round(rating);
-            }
-        });
-
-        health.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                healthUserRating = Math.round(rating);
-            }
-        });
-
-        fairAndSocial.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                fairAndSocialUserRating = Math.round(rating);
-            }
-        });
-
-        animalTreatment.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                animalTreatmentUserRating = Math.round(rating);
-            }
-        });
-
-        toshop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               // Intent searchbar = new Intent(UserValues.this, ShoppingSearchBar.class);
-               // startActivity(searchbar);
-                //Searchproducts ApiObj2 = new Searchproducts(environmentUserRating,healthUserRating,fairAndSocialUserRating,animalTreatmentUserRating);
-              //  Obj2.setUserValues(environmentUserRating,healthUserRating,fairAndSocialUserRating,animalTreatmentUserRating);
-                Intent searchbar = new Intent(UserValues.this, Searchproducts.class);
-                startActivity(searchbar);
-            }
-        });
-    }
-}
+//package com.example.youcare;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.RatingBar;
+//import android.widget.TextView;
+//
+//import com.example.youcare.appbody.search.Searchproducts;
+//
+//public class UserValues extends AppCompatActivity {
+//
+//    public RatingBar environement, animalTreatment, health, fairAndSocial;
+//    public android.widget.SeekBar price;
+//    public Button toshop;
+//    int environmentUserRating, animalTreatmentUserRating, healthUserRating, fairAndSocialUserRating;
+//
+//
+////@parameter userPriceInput refers to the input from the user and @parameter priceOfpurchase to show the price on display
+//    public int userPriceInput=0;  public TextView priceOfpurchase;
+//    Searchproducts Obj2 = new Searchproducts();
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_user_values);
+//        environement = findViewById(R.id.environmentRatingbar);
+//        animalTreatment = findViewById(R.id.animaltreatmentRating);
+//        health = findViewById(R.id.HealthId);
+//        fairAndSocial = findViewById(R.id.FairAndSocial);
+//        price = (android.widget.SeekBar) findViewById(R.id.PriceSeekbar);
+//        toshop = findViewById(R.id.shoppingButton);
+//        priceOfpurchase = findViewById(R.id.priceView);
+//
+//        //priceOfpurchase.setText(userPriceInput)
+//
+//
+//        price.setOnSeekBarChangeListener(new android.widget.SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(android.widget.SeekBar seekBar, int progress, boolean fromUser) {
+//                userPriceInput = progress;
+//
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(android.widget.SeekBar seekBar) {
+//
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(android.widget.SeekBar seekBar) {
+//                String temp = Integer.toString(userPriceInput);
+//                priceOfpurchase.setText(temp);
+//
+//            }
+//       });
+//
+//        environement.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+//            @Override
+//            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+//                environmentUserRating = Math.round(rating);
+//            }
+//        });
+//
+//        health.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+//            @Override
+//            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+//                healthUserRating = Math.round(rating);
+//            }
+//        });
+//
+//        fairAndSocial.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+//            @Override
+//            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+//                fairAndSocialUserRating = Math.round(rating);
+//            }
+//        });
+//
+//        animalTreatment.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+//            @Override
+//            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+//                animalTreatmentUserRating = Math.round(rating);
+//            }
+//        });
+//
+//        toshop.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//               // Intent searchbar = new Intent(UserValues.this, ShoppingSearchBar.class);
+//               // startActivity(searchbar);
+//                //Searchproducts ApiObj2 = new Searchproducts(environmentUserRating,healthUserRating,fairAndSocialUserRating,animalTreatmentUserRating);
+//              //  Obj2.setUserValues(environmentUserRating,healthUserRating,fairAndSocialUserRating,animalTreatmentUserRating);
+//                Intent searchbar = new Intent(UserValues.this, Searchproducts.class);
+//                startActivity(searchbar);
+//            }
+//        });
+//    }
+//}
